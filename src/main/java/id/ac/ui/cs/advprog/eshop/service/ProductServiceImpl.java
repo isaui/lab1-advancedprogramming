ef0f2aa0dbe52f5992c028d274df7db7613666a3
isaui <isadestroyed17@gmail.com>
2024-02-02 09:17:02 +0700

    ya mantap menambahkan controller dan template

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductServiceImpl.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
+@Service
 public class ProductServiceImpl implements ProductService{
     @Autowired
     private ProductRepository productRepository;
