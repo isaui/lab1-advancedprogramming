f5b9ee8e5fb8001ae750d137ff61d5e959cbb398
isaui <isadestroyed17@gmail.com>
2024-02-13 21:22:55 +0700

    Delete unnecessary localvar

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductServiceImpl.java
@@ -27,8 +27,7 @@ public class ProductServiceImpl implements ProductService{
     
     @Override
     public Product findById(String productId){
-        Product product = productRepository.findById(productId);
-        return product;
+        return productRepository.findById(productId);
     }
 
     @Override
