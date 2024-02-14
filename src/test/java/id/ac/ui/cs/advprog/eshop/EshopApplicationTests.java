905b766fc29c7fe71816316a6c1fa2a6d940c773
isaui <isadestroyed17@gmail.com>
2024-02-14 18:03:48 +0700

    update test case on springapplicationrun

--- src/test/java/id/ac/ui/cs/advprog/eshop/EshopApplicationTests.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/EshopApplicationTests.java
@@ -1,15 +1,10 @@
 package id.ac.ui.cs.advprog.eshop;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.fail;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.*;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.ConfigurableApplicationContext;
+
 
 @SpringBootTest
 class EshopApplicationTests {
@@ -25,7 +20,7 @@ class EshopApplicationTests {
 
     @Test
     void testSpringApplicationRun() {
-        EshopApplication.main(new String[] {});
+         assertDoesNotThrow(() -> EshopApplication.main(new String[] {}));
 
     }
 }
