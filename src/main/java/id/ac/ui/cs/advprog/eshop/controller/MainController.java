1a640f79da0ddf3810e7ade878000b3c12894340
isaui <isadestroyed17@gmail.com>
2024-02-14 19:05:56 +0700

    fix case sensitive url on controller  and finish module 2

--- src/main/java/id/ac/ui/cs/advprog/eshop/controller/MainController.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/controller/MainController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class MainController {
     @GetMapping("/")
     public String getMain() {
-        return "homePage";
+        return "HomePage";
     }
     
 }
