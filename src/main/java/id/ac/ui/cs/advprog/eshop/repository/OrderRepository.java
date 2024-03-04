fba86b403f46c767b7988bd211590a45120ea5b8
isaui <isadestroyed17@gmail.com>
2024-03-05 06:09:52 +0700

    [GREEN] Implement OrderRepository class

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/OrderRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/OrderRepository.java
@@ -12,10 +12,37 @@ import id.ac.ui.cs.advprog.eshop.model.Order;
 public class OrderRepository {
     private List<Order> orderData = new ArrayList<>();
 
-    public Order save(Order order){return null;}
+    public Order save(Order order){
+        int i = 0;
+        for(Order savedOrder: orderData){
+            if(savedOrder.getId().equals(order.getId())){
+                orderData.remove(i);
+                orderData.add(i, order);
+                return order;
+            }
+            i+=1;
+        }
+        orderData.add(order);
+        return order;
+    }
 
-    public Order findById(String id){return null;}
+    public Order findById(String id){
+        for(Order savedOrder: orderData){
+            if(savedOrder.getId().equals(id)){
+                return savedOrder;
+            }
+        }
+        return null;
+    }
 
-    public List<Order> findAllByAuthor(String author){return null;}
+    public List<Order> findAllByAuthor(String author){
+        List<Order> result = new ArrayList<>();
+        for(Order savedOrder : orderData){
+            if(savedOrder.getAuthor().equals(author)){
+                result.add(savedOrder);
+            }
+        }
+        return result;
+    }
     
 }
